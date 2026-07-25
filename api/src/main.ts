81eaf2a2442e42bcd8b3d518dafd7aeb904a9787
ronishai <ronishai416@gmail.com>
2026-07-25 18:50:35 +0300

    remove api prefix

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,8 +6,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
